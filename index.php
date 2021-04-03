1f44f1d4fceb60ff4d8d9c04f794991ed6a192c3
haido23 <ngochai2519@gmail.com>
2021-04-03 13:21:40 +0700

    thay doi lab

--- index.php
+++ index.php
@@ -1,14 +1,14 @@
 <?php
-require_once("../header.php");
+require_once("header.php");
 ?>
 
 <form action="" method="post">
-    Tên: <input type="text" name="lblUsername" id="" placeholder="txtUsername"><br>
-    Mật Khẩu: <input type="text" name="lblPassword" id="" placeholder="txtPassword"><br>
+<label>UserName</label> <input type="text" name="lblUsername" id="" placeholder="txtUsername"><br>
+<label>PassWord</label> <input type="text" name="lblPassword" id="" placeholder="txtPassword"><br>
 
     <input type="submit" value="Them">
 </form>
 
 <?php
-require_once("../footer.php");
+require_once("footer.php");
 ?>
