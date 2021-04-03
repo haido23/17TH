00d8d0618ec29e560c3d6bf7948bac2320b9964a
haido23 <ngochai2519@gmail.com>
2021-04-03 13:11:24 +0700

    them pass

--- index.php
+++ index.php
@@ -4,6 +4,7 @@ require_once("../header.php");
 
 <form action="" method="post">
     Tên: <input type="text" name="lblUsername" id="" placeholder="txtUsername"><br>
+    Mật Khẩu: <input type="text" name="lblPassword" id="" placeholder="txtPassword"><br>
 
     <input type="submit" value="Them">
 </form>
