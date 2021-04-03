4ac588d53d2ab7135823b65b2993fde6da511066
pltuanhuy <pltuanhuy@gmail.com>
2021-04-03 13:07:20 +0700

    update input tài khoản

---
+++ header.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<style>
+/* #main{
+
+}
+
+ul>li{
+    background: cornsilk;
+    padding:
+} */
+</style>
+<body>
+    <div id="main">
+        <div id="menu">
+            <ul>
+                <li>
+                    <a href="/bai3/Views/TaoTaiKhoan.php">Tạo tài khoản</a>
+                </li>
+                <li>
+                    <a href="/bai3/Controllers/TaiKhoanController.php?action=danhsach">Xem tài khoản</a>
+                </li>
+            </ul>
+        </div>
+    </div>
+<body>
--- index.php
+++ index.php
@@ -1,1 +1,13 @@
-<h1>Proplayer<h1>
+<?php
+require_once("../header.php");
+?>
+
+<form action="" method="post">
+    Tên: <input type="text" name="lblUsername" id="" placeholder="txtUsername"><br>
+
+    <input type="submit" value="Them">
+</form>
+
+<?php
+require_once("../footer.php");
+?>
