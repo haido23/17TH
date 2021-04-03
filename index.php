b378b9471d742ff008c448b0ec44d729d0386981
doquocbao <beo.supercat140@gmail.com>
2021-04-03 13:38:34 +0700

    commit validate password and admin

--- index.php
+++ index.php
@@ -1,12 +1,42 @@
 <?php
 require_once("header.php");
+if (isset($_POST['btn-sign-in'])) {
+
+    $u_name = $_POST['txtUsername'];
+    $u_pass = $_POST['txtPassword'];
+    $password = md5($u_pass);
+
+    $result = 1;
+    if ($u_name == ""  || $u_pass == "") {
+?>
+        <script>
+            alert('Please fully fill in !');
+        </script>
+        <?php
+    } else {
+        if ($u_name == "admin" && $u_pass == "admin") {
+        ?>
+            <script>
+                alert('Login succesful!!');
+            </script>
+        <?php
+        } else {
+        ?>
+            <script>
+                alert('Login unsuccesful!!');
+            </script>
+<?php
+        }
+    }
+}
+
 ?>
 
 <form action="" method="post">
-<label>UserName</label> <input type="text" name="lblUsername" id="" placeholder="txtUsername"><br>
-<label>PassWord</label> <input type="text" name="lblPassword" id="" placeholder="txtPassword"><br>
+    <label>UserName</label> <input type="text" name="txtUsername" id="" placeholder="txtUsername"><br>
+    <label>PassWord</label> <input type="password" name="txtPassword" id="" placeholder="txtPassword"><br>
 
-    <input type="submit" value="Them">
+    <input type="submit" name="btn-sign-in" value="Sign in">
 </form>
 
 <?php
