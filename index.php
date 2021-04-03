22e8568c5b5d12383e13abdf732dec0cae485011
pltuanhuy <pltuanhuy@gmail.com>
2021-04-03 13:26:24 +0700

    Da them button login

--- index.php
+++ index.php
@@ -3,8 +3,8 @@ require_once("header.php");
 ?>
 
 <form action="" method="post">
-<label>UserName</label> <input type="text" name="lblUsername" id="" placeholder="txtUsername"><br>
-<label>PassWord</label> <input type="text" name="lblPassword" id="" placeholder="txtPassword"><br>
+    Tên: <input type="text" name="lblUsername" id="" placeholder="txtUsername"><br>
+    Mật Khẩu: <input type="text" name="lblPassword" id="" placeholder="txtPassword"><br>
 
     <input type="submit" value="Login">
 </form>
